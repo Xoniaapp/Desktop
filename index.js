9fd0d8d86fdd0e62f95986ae72339bda59cfe7a1
Aelpxy <84912564+Aelpxy@users.noreply.github.com>
2022-02-01 09:13:30 +0000

    Add files via upload

---
+++ index.js
@@ -0,0 +1,30 @@
+const { app, BrowserWindow } = require('electron')
+const path = require('path')
+
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    icon: __dirname + './icons/logo.png',
+  })
+  win.setOverlayIcon('./icons/logo.png', 'Xonia')
+  win.setIcon(path.join(__dirname, './icons/logo.png'));
+  win.loadURL("https://app.xoniaapp.com/channels/me")
+}
+
+app.whenReady().then(() => {
+  createWindow()
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow()
+    }
+  })
+})
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
