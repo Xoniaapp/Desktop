cf7514d71d1b2535fcb886304339644fc0874990
Aelpxy <84912564+Aelpxy@users.noreply.github.com>
2022-03-07 22:03:58 +0600

    fix: res

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
 const { app, BrowserWindow } = require('electron')
 const path = require('path')
 
-
 function createWindow() {
   const win = new BrowserWindow({
-    width: 800,
-    height: 600,
+    width: 1280,
+    height: 720,
     icon: __dirname + '/icons/logo.png',
     autoHideMenuBar: true,
   })
